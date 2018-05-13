45c96c35421f7f987ef3caa1847f6a75f4c4bc57
Alexander Ryabkov <alexryabkov@gmail.com>
2018-05-13 15:58:42 +0300

    first commit

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='mycut')
+def mycut(value, arg):
+    """This cuts out all values of "arg" from the string!"""
+    return value.replace(arg, '')
